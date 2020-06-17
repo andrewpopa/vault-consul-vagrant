8a01af7f78011a7823231fd5ee5444e3f0c53844
Andrei Popa <andrei@hashicorp.com>
2020-06-17 20:57:09 +0200

    add comment, fixing vagrant file, add separate unseal

--- Vagrantfile
+++ Vagrantfile
@@ -12,7 +12,7 @@ Vagrant.configure("2") do |config|
     config.vm.define vm_name="consul#{i}" do |node|
       node.vm.box = "apopa/bionic64"
       node.vm.hostname = vm_name
-      node.vm.network "public_network", ip: "192.168.178.#{30+i}"
+      node.vm.network "public_network", ip: "192.168.178.#{30+i}", bridge: "wlp0s20f3"
       node.vm.provision "shell", path: "scripts/consul_server.sh"
     end
   end
@@ -20,9 +20,10 @@ Vagrant.configure("2") do |config|
   config.vm.define vm_name="vault" do |node|
     node.vm.box = "apopa/bionic64"
     node.vm.hostname = vm_name
-    node.vm.network "public_network", ip: "192.168.178.40"
+    node.vm.network "public_network", ip: "192.168.178.40", bridge: "wlp0s20f3"
     node.vm.provision "shell", path: "scripts/consul_client.sh"
     node.vm.provision "shell", path: "scripts/vault_server.sh"
+    node.vm.provision "shell", path: "scripts/vault_server.sh", privileged: false
   end
   
 end
--- scripts/vault_server.sh
+++ scripts/vault_server.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
+# we want the scrip to be verbose
 set -x
 
+# install packages if not installed
 which curl wget unzip jq &>/dev/null || {
   export DEBIAN_FRONTEND=noninteractive
   sudo apt-get update
   sudo pt-get install --no-install-recommends -y curl wget unzip jq
 }
 
-VAULT=$(curl -sL https://releases.hashicorp.com/vault/index.json | jq -r '.versions[].version' | sort -V | egrep -v 'ent|beta|rc|alpha' | tail -n1)
+# get current version of consul that is not beta/alpha
+VAULTVER=$(curl -sL https://releases.hashicorp.com/vault/index.json | jq -r '.versions[].version' | sort -V | egrep -v 'ent|beta|rc|alpha' | tail -n1)
 
+# check if vault is installed
+# if not, download and configure service
 which vault &>/dev/null || {
 
   which curl wget unzip jq &>/dev/null || {
@@ -18,19 +23,28 @@ which vault &>/dev/null || {
     sudo apt-get install --no-install-recommends -y curl wget unzip jq
   }
 
-  echo Installing vault ${VAULT}
-  wget https://releases.hashicorp.com/vault/${VAULT}/vault_${VAULT}_linux_amd64.zip
-  unzip vault_${VAULT}_linux_amd64.zip
+  echo Installing vault ${VAULTVER}
+  wget https://releases.hashicorp.com/vault/${VAULTVER}/vault_${VAULTVER}_linux_amd64.zip
+  unzip vault_${VAULTVER}_linux_amd64.zip
   sudo mv vault /usr/local/bin/
   vault -autocomplete-install
   sudo setcap cap_ipc_lock=+ep /usr/local/bin/vault
+
+  # create vault user
   sudo useradd --system --home /etc/vault.d --shell /bin/false vault
+
+  # copy vault configuration
   sudo cp /vagrant/conf/vault.service /etc/systemd/system/vault.service
   sudo mkdir -p /etc/vault
   sudo cp /vagrant/conf/vault_server.hcl /etc/vault/vault_server.hcl
-  sudo sed -i "s/localhost/$(hostname -I | awk '{print $2}')/g" /etc/vault/vault_server.hcl
+  
+  # adjust vault config
+  sudo sed -i "s/localhost/$(hostname -I | grep -E -o "(192)\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})")/g" /etc/vault/vault_server.hcl
+
+  # enable and start service  
   sudo systemctl enable vault
   sudo systemctl start vault
   export VAULT_ADDR=http://127.0.0.1:8200
-  vault status
+  # status will fail as its sealed
+  vault status || true
 }
